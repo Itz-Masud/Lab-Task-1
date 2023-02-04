0b44fdcd0877d08766575c33a3ea7ae817708016
Itz-Masud <mhmasud507@gmail.com>
2023-02-04 20:40:59 +0600

    Final Touch No 1

--- Lab Task 1/LabTask1.java
+++ Lab Task 1/LabTask1.java
@@ -7,6 +7,10 @@ public class LabTask1 {
     public static void main(String[] args) {
         
         System.out.println("Mahamudul Hasan Masud");
+        System.out.println("ID:221-15-5206");
+        System.out.println("Dept:BSc in CSE");
+        System.out.println("Address:Saidpur,Bangladesh");
+        System.out.println("Favourite Game:Cricket");
         
     }
 }
