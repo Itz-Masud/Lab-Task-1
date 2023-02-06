745a05f072cfcf95efec1fd2128117a4837f5be7
Itz-Masud <mhmasud507@gmail.com>
2023-02-06 20:47:17 +0600

    3rd

---
+++ Lab Task 1/Name.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.name;
+
+
+public class Name {
+
+    public static void main(String[] args) {
+        String Name =("Mahamudul Hasan Masud");
+        System.out.println(Name);
+    }
+}
