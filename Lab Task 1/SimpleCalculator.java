f0ad954a1a94094a7aedb91865a4926623109d07
Itz-Masud <mhmasud507@gmail.com>
2023-02-06 20:55:11 +0600

    2nd

---
+++ Lab Task 1/SimpleCalculator.java
@@ -0,0 +1,23 @@
+
+
+package com.mycompany.simplecalculator;
+
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+       int a,b;
+        a=24;
+        b=17;
+        
+        int Summation = a+b;
+        int Subtraction = a-b;
+        int Multiplication = a*b;
+        int Division = a/b;
+        
+        System.out.println("Summation:" +Summation);
+        System.out.println("Subtraction:" +Subtraction);
+        System.out.println("Multiplication:" +Multiplication);
+        System.out.println("Division:" +Division);
+    }
+}
