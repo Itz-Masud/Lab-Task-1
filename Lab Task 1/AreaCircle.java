d4af86a271311878980d7b10f6d63b0010b6eeed
Itz-Masud <mhmasud507@gmail.com>
2023-02-06 20:44:16 +0600

    4rd

---
+++ Lab Task 1/AreaCircle.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.areacircle;
+
+
+public class AreaCircle {
+
+    public static void main(String[] args) {
+       double pi =3.1416,area;
+        int r=2;
+        area=pi*r*r;
+        System.out.printf("Area of Circle is : %.2f\n", area);
+    }
+}
